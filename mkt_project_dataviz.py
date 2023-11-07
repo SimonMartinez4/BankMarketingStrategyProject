c2fed320d05f2736b624ce5f69b4c5946dd683a3
SimonMartinez4 <134703285+SimonMartinez4@users.noreply.github.com>
2023-11-07 18:55:01 +0100

    Add files via upload

--- mkt_project_dataviz.py
+++ mkt_project_dataviz.py
@@ -7,7 +7,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 from sklearn.model_selection import train_test_split
